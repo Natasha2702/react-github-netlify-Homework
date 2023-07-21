8587a10c18999a275e5626bb7f236c5afa783ec0
Natasha2702 <Moyanenatasha@gmail.com>
2023-07-21 11:56:20 +0200

    Initial file structure and contents

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Weather from ".src/Weather";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Weather />
+    </div>
+  );
+}
